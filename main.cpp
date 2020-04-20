44ca3eb607f3fc45c532be6bed2e0c5893407d88
aczech17 <adrianczech-1997@wp.pl>
2020-04-20 17:20:38 +0200

    oop

--- main.cpp
+++ main.cpp
@@ -4,90 +4,106 @@
 
 using std::cin, std::cout, std::endl, std::cerr;
 
-HANDLE hComm;
-DCB serialConfig = {0};
-struct joystickPosition
+class Port
 {
-    int x, y;
+    HANDLE hComm;
+    DCB serialConfig = {0};
+    const char* portName;
+    DWORD BaudRate;
+public:
+    struct Position
+    {
+        int x;
+        int y;
+    };
+private:
+    std::string readLine()
+    {
+        char tempChar;
+        std::string buffer;
+        DWORD countBytesRead = 0;
+        do
+        {
+            ReadFile(hComm, &tempChar, sizeof(tempChar), &countBytesRead, nullptr);
+            buffer += tempChar;
+        }while(tempChar != '\n');
+        return buffer;
+    }
+
+    Position getPosFromStr(std::string str)
+    {
+        Position result = {0, 0};
+        int i = 0;
+        //ignore artifacts
+        while(str[i] < '0' || str[i] > '9')
+            i++;
+        //first number
+        int j = i;
+        while(str[j] != ' ')
+            j++;
+        std::string str1 = str.substr(i, j - i);
+        result.x = std::stoi(str1);
+
+        //second number
+        j++;
+
+        std::string str2 = str.substr(j, str.size() - j - 1);// no copy endl (no no no, why you copy my movies?)
+        result.y = std::stoi(str2);
+
+        return result;
+    }
+public:
+
+    explicit Port(const char* portNameParam = "COM3", DWORD BaudRateParam = CBR_9600) : portName(portNameParam), BaudRate(BaudRateParam)
+    {
+        hComm = CreateFile(portName, GENERIC_READ, 0, nullptr, OPEN_EXISTING, 0, nullptr);
+        if (hComm == INVALID_HANDLE_VALUE)
+        {
+            std::string errMsg = "Cannot open port ";
+            errMsg += portName;
+            throw(std::ios_base::failure(errMsg));
+        }
+
+        serialConfig.DCBlength = sizeof(serialConfig); // ???
+        serialConfig.BaudRate = CBR_9600;
+        serialConfig.ByteSize = 8;
+        serialConfig.StopBits = ONESTOPBIT;
+        serialConfig.Parity = NOPARITY;
+    }
+
+    ~Port()
+    {
+        CloseHandle(hComm);
+    }
+
+    Position getPosition()
+    {
+        std::string str = readLine();
+        return getPosFromStr(str);
+    }
 };
 
-void openPort(const char* port)
+
+struct JoystickPosition
 {
-    hComm = CreateFile(port, GENERIC_READ, 0, nullptr, OPEN_EXISTING, 0, nullptr);
-    if (hComm == INVALID_HANDLE_VALUE)
+    int x, y;
+    JoystickPosition& operator=(Port::Position pos)
     {
-        cerr << ("Perkele :DDD") << endl;
-        exit(1);
+        x = pos.x;
+        y = pos.y;
+        return *this;
     }
-    else
-        cout << ("Otwarto port ") << port << endl;
-
-    serialConfig.DCBlength = sizeof(serialConfig); // ???
-    serialConfig.BaudRate = CBR_9600;
-    serialConfig.ByteSize = 8;
-    serialConfig.StopBits = ONESTOPBIT;
-    serialConfig.Parity = NOPARITY;
-}
-
-std::string readLine(const char* port)
-{
-    char tempChar;
-    std::string buffer = "";
-    DWORD countBytesRead = 0;
-    do
-    {
-        ReadFile(hComm, &tempChar, sizeof(tempChar), &countBytesRead, nullptr);
-        buffer+=tempChar;
-    }while(tempChar != '\n');
-    return buffer;
-}
-
-joystickPosition getPosFromStr(std::string str)
-{
-    joystickPosition result = {0, 0};
-    int i = 0;
-    //ignore artifacts
-    while(str[i] < '0' || str[i] > '9')
-        i++;
-    //first number
-    int j = i;
-    while(str[j] != ' ')
-        j++;
-    std::string str1 = str.substr(i, j - i);
-    result.x = std::stoi(str1);
-
-    //second number
-    j++;
-
-    std::string str2 = str.substr(j, str.size() - j - 1);// no copy endl (no no no, why you copy my movies?)
-    result.y = std::stoi(str2);
-
-    return result;
-}
-
-joystickPosition getOneRecord(const char* port)
-{
-    std::string str = readLine(port);
-    return getPosFromStr(str);
-}
+};
 
 int main(int argc, char **argv)
 {
-    char *port;
-    if(argc > 1)
-        port = argv[1];
-    else
-        port = (char*)"COM3";
-
-    openPort(port);
-
+    Port p1;
+    JoystickPosition pos = {0, 0};
     for(int i = 0; i < 20; ++i)
     {
-        joystickPosition position = getOneRecord(port);
-        cout<<"x: " << position.x << " ";
-        cout<<"y: " << position.y << endl;
+        pos = p1.getPosition();
+        cout<<"x: " << pos.x << " y: " << pos.y << endl;
     }
 
-    CloseHandle(hComm);
     return 0;
 }
